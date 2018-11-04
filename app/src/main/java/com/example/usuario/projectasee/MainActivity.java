0de3f67c2fae429ea3cf46fedb85fbdeec6546a8
Candido <caguilary@alumnos.unex.es>
2018-11-04 20:46:07 +0100

    added tabs and toolbar to list_rutes

--- app/src/main/java/com/example/usuario/projectasee/MainActivity.java
+++ app/src/main/java/com/example/usuario/projectasee/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar ( toolbar );
     }
 
-    public boolean onCreateOptionMenu(Menu menu){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater ().inflate ( R.menu.menu_main ,menu );
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId ();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.ic_action_setting) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected ( item );
     }
 }
