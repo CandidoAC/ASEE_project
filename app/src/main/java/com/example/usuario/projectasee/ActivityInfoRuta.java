5dc48d24ad16cc43342ebf91ecc8b0310e1130f9
Candido <caguilary@alumnos.unex.es>
2018-11-16 16:39:47 +0100

    mapa en info_rutas

--- app/src/main/java/com/example/usuario/projectasee/ActivityInfoRuta.java
+++ app/src/main/java/com/example/usuario/projectasee/ActivityInfoRuta.java
@@ -1,9 +1,15 @@
 package com.example.usuario.projectasee;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,11 +17,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class ActivityInfoRuta extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityInfoRuta extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    private Marker marker;
+    private MapView mMapView;
+    private double lat=0.0;
+    private double lon=0.0;
+    private static final int PETICION_PERMISO_LOCALIZACION=101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.rutaactivity );
+        mMapView = (MapView) findViewById ( R.id.mapView2 );
+        mMapView.onCreate (savedInstanceState );
+
+        mMapView.onResume (); // needed to get the map to display immediately
 
         Toolbar toolbar = (Toolbar) findViewById ( R.id.toolbar );
         toolbar.setNavigationIcon ( R.drawable.ic_arrow );
@@ -26,6 +54,15 @@ public class ActivityInfoRuta extends AppCompatActivity {
             }
         } );
         setSupportActionBar ( toolbar );
+
+        try {
+            MapsInitializer.initialize ( getApplicationContext () );
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+
+
+        mMapView.getMapAsync ( this);
     }
 
     @Override
@@ -73,4 +110,102 @@ public class ActivityInfoRuta extends AppCompatActivity {
         }
         return true;
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        miUbicacion ();
+    }
+
+    public void anadirMarker(double lat , double lon) {
+        LatLng coord = new LatLng ( lat , lon );
+        CameraUpdate ub = CameraUpdateFactory.newLatLngZoom ( coord , 16 );
+        if (marker != null) {
+            marker.remove ();
+        }
+        marker = googleMap.addMarker ( new MarkerOptions ()
+                .position ( coord )
+                .title ( "Mi ubicación" )
+                .icon ( BitmapDescriptorFactory.fromResource ( R.mipmap.icono ) ) );
+        googleMap.animateCamera ( ub );
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     *//*
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }*/
+
+    public void actualizarUb(Location location) {
+        if (location != null) {
+            lat = location.getLatitude ();
+            lon = location.getLongitude ();
+            anadirMarker ( lat , lon );
+        }
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUb ( location );
+        }
+
+        @Override
+        public void onStatusChanged(String provider , int status , Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        } else {
+
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER );
+            actualizarUb(location);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER , 15000,0,locListener);
+        }
+    }
+
 }
